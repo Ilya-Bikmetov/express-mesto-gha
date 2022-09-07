714020e7f866b0b8246c98b1f0891876bd903819
Ilya Bikmetov <ilya-bikmetov@yandex.ru>
2022-09-07 21:53:58 +0700

    ref/check_validation_createUser

--- middlewares/validators.js
+++ middlewares/validators.js
@@ -7,9 +7,9 @@ const { avatarRegExp, cardRegExp } = require('../utils/constants');
 
 const createUserValidator = celebrate({
   [Segments.BODY]: Joi.object().keys({
-    name: Joi.string().required().min(2).max(30),
-    about: Joi.string().required().min(2).max(30),
-    avatar: Joi.string().required().regex(avatarRegExp),
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+    avatar: Joi.string().regex(avatarRegExp),
     email: Joi.string().required().email(),
     password: Joi.string().required(),
 
